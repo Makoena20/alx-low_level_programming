09d907b40e8addcf08d3686f20bccd0b2826fa3a
Makoena20 <koenaletseku@gmail.com>
2023-11-01 06:14:34 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,13 +1,13 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes a character to the standard output (stdout)
+ * _putchar - write a character to stdout
  * @c: The character to print
  *
- * Return: On success, the number of characters written. On error, -1.
+ * Return: 1 on success, -1 on error
  */
 int _putchar(char c)
 {
-	return write(1, &c, 1);
+    return write(1, &c, 1);
 }
 
