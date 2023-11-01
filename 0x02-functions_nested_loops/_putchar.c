3ac9d980f091c8a187e03acd2010c45590ebb9ba
Makoena20 <koenaletseku@gmail.com>
2023-11-01 05:58:25 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,13 +1,13 @@
 #include <unistd.h>
 
 /**
- * _putchar - Write a character to the standard output (stdout).
- * @c: The character to write.
+ * _putchar - writes a character to the standard output (stdout)
+ * @c: The character to print
  *
- * Return: On success, 1. On error, -1 is returned, and errno is set appropriately.
+ * Return: On success, the number of characters written. On error, -1.
  */
 int _putchar(char c)
 {
-    return write(1, &c, 1);
+	return write(1, &c, 1);
 }
 
