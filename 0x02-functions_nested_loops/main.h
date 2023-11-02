d921a1f9c631993ba2b2860741311e19fa1dca31
Makoena20 <koenaletseku@gmail.com>
2023-11-02 02:18:01 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _abs(int n);
+int _putchar(char c);
+int print_last_digit(int);
+/* Add any additional function prototypes or declarations here if needed */
 
 #endif /* MAIN_H */
 
