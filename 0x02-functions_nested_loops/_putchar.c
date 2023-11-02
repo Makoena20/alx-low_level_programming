114f46f81ea3bf1243bff91828e75665af504f2c
Makoena20 <koenaletseku@gmail.com>
2023-11-02 06:02:12 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,12 +1,5 @@
 #include <unistd.h>
 
-/**
- * _putchar - Writes a character to the standard output (stdout).
- * @c: The character to be printed.
- *
- * Return: On success, the number of characters written is returned.
- * On error, -1 is returned, and errno is set appropriately.
- */
 int _putchar(char c)
 {
     return write(1, &c, 1);
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+void times_table(void);
 int _putchar(char c);
-void jack_bauer(void);
 
-#endif /* MAIN_H */
+#endif
 
