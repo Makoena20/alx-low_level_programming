c5cdb433db227c2abfc6df1b9331bb254a06efcb
Makoena20 <koenaletseku@gmail.com>
2023-11-02 12:13:38 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,13 +2,9 @@
 #define MAIN_H
 
 int _isupper(int c);
-
-#endif /* MAIN_H */
-
-#ifndef MAIN_H
-#define MAIN_H
-
 int _isdigit(int c);
+int mul(int a, int b);
+int _putchar(char c);
+void print_numbers(void);
 
 #endif /* MAIN_H */
-
