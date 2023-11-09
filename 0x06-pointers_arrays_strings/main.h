581eccf7bc1291616110b16ceaed006137be0bb8
Makoena20 <koenaletseku@gmail.com>
2023-11-09 05:34:23 -0800

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -13,5 +13,6 @@ char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
+char *string_toupper(char *);
 
 #endif /* MAIN_H */
