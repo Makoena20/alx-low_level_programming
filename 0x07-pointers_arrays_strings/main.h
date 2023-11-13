ac6428c0622d3f71c3fb42c29b6c1a824fbdc1b6
Makoena20 <koenaletseku@gmail.com>
2023-11-13 04:11:19 -0800

    1-memcpy.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,8 +2,11 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <stddef.h>
 
 char *_memset(char *s, char b, unsigned int n);
 void simple_print_buffer(char *buffer, unsigned int size);
+void simple_print_buffer(char *buffer, unsigned int size);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 #endif /* MAIN_H */
