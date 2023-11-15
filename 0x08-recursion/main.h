12959b440778dcd458aafbb7c9c4b44fa5d5616c
Makoena20 <koenaletseku@gmail.com>
2023-11-14 23:58:03 -0800

    4-pow_recursion.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,6 +8,8 @@ void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int _putchar(char c);
 int factorial(int n);
+int _putchar(char c);
+int _pow_recursion(int x, int y);
 
 #endif /* MAIN_H */
 
