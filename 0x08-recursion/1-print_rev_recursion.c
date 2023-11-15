095130f863ffa668e385538e5eaccbdac5ca1b3e
Makoena20 <koenaletseku@gmail.com>
2023-11-14 22:53:31 -0800

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,13 +2,14 @@
 
 /**
  * _print_rev_recursion - prints a string in reverse
- * @s: the string to be printed in reverse
+ * @s: string to be printed
+ *
+ * Return: void
  */
 void _print_rev_recursion(char *s)
 {
-    if (*s)
-    {
-        _print_rev_recursion(s + 1);
-        _putchar(*s);
-    }
+	if (*s == '\0')
+		return;
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
