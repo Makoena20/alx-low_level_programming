78a4174f1cfa65a2d0f1f53ed3221dc891e5677e
Makoena20 <koenaletseku@gmail.com>
2023-11-15 00:47:43 -0800

    5-sqrt_recursion.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,6 +10,10 @@ int _putchar(char c);
 int factorial(int n);
 int _putchar(char c);
 int _pow_recursion(int x, int y);
+int _putchar(char c);
+int is_prime_number(int n);
+int _sqrt_recursion(int n);
+int find_sqrt(int n, int i);
 
 #endif /* MAIN_H */
 
