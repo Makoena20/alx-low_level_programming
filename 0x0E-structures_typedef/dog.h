93a836986629d13bf432d9af4580eee466de5133
Makoena20 <koenaletseku@gmail.com>
2023-11-28 10:56:11 -0800

    4-new_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -30,5 +30,6 @@ dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif /* DOG_H */
