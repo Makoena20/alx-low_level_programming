9f848d705dfaae12c7229ced217387aaafcee9b3
Makoena20 <koenaletseku@gmail.com>
2023-11-30 05:26:20 -0800

    3-print_all.c

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,5 +7,13 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 
+typedef struct print_formats
+{
+	char *format;
+	void (*print_func)(va_list);
+} print_formats_t;
+
+void print_all(const char * const format, ...);
+
 #endif /* VARIADIC_FUNCTIONS_H */
 
