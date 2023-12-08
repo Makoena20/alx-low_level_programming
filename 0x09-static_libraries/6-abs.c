28b758607865c860091a4b73f999142d8f2df69b
Makoena20 <koenaletseku@gmail.com>
2023-12-08 05:51:24 -0800

    create_static_lib.sh

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+int _abs(int n)
+{
+    return abs(n);
+}
+
--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,10 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>  // Include any necessary headers here
-
-// Declare function prototypes or other necessary declarations here
-
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -27,3 +23,4 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
 #endif /* MAIN_H */
+
