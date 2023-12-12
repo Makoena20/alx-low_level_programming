0624f9b28e0a0377f6a60fe636a8cb19539dbe7d
Makoena20 <koenaletseku@gmail.com>
2023-12-12 04:54:54 -0800

    103-find_loop.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -56,6 +56,10 @@ size_t print_listint_safe(const listint_t *head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *add_nodeint(listint_t **head, const int n);
+size_t print_listint_safe(const listint_t *head);
+size_t free_listint_safe(listint_t **h);
+listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *find_listint_loop(listint_t *head);
 
 #endif /* LISTS_H */
 
