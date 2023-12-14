6506512b229f942e26c4325a3a1aef32b1e07c5f
Makoena20 <koenaletseku@gmail.com>
2023-12-13 23:07:05 -0800

    4-clear_bit.c

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,11 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+
 unsigned int binary_to_uint(const char *b);
 int _putchar(char c);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
 
 #endif /* MAIN_H */
 
