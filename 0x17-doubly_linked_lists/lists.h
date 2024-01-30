89e30675461debe8cf20205afad724746e77353e
Makoena20 <koenaletseku@gmail.com>
2024-01-30 03:10:22 -0800

    1-dlistint_len.c

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -25,6 +25,7 @@ typedef struct dlistint_s
  * Return: The number of nodes in the list
  */
 size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
 
 #endif /* LISTS_H */
 
